fab42150f69b14ffd6271436ccde084e4f401d24
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-09-23 00:29:23 +0600

    Donation Event [added]

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
 document.getElementById("donation-btn")
-.addEventListener("click", function() {
-    document.getElementById("history-div").classList.add("hidden");
-    document.getElementById("donation-div").classList.remove("hidden");
-})
+    .addEventListener("click", function () {
+        document.getElementById("history-div").classList.add("hidden");
+        document.getElementById("donation-div").classList.remove("hidden");
+    })
 
 document.getElementById("history-btn")
-.addEventListener("click", function() {
-    document.getElementById("history-div").classList.remove("hidden");
-    document.getElementById("donation-div").classList.add("hidden");
+    .addEventListener("click", function () {
+        document.getElementById("history-div").classList.remove("hidden");
+        document.getElementById("donation-div").classList.add("hidden");
+    })
 
-})
+// donation
+document.getElementById("donate-btn-1").
+    addEventListener("click", function () {
+        const event = document.getElementById("event-1").innerText;
+        const amount = document.getElementById("donation-1").value;
+        const oldBalance = document.getElementById("balance-amount").innerText;
+        const newBalance = parseInt(oldBalance) + parseInt(amount);
+        document.getElementById("balance-amount").innerText = newBalance;
+        document.getElementById("event-1").innerText = parseFloat(event) + parseFloat(amount);
+        
+        // show modal
+        document.getElementById("my_modal_1").showModal()
+    })
+    
